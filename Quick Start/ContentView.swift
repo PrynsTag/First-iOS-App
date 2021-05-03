8430bbd2a849e0d0208f3782fd26ee47fd2a82c0
PrynsTag <princevelasco16@gmail.com>
2021-05-04 00:35:16 +0800

    Added images to view.

--- Quick Start/ContentView.swift
+++ Quick Start/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background").ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack {
+                    VStack {
+                        Text("Player").padding().colorInvert()
+                        Text("0").colorInvert()
+                    }
+                    VStack {
+                        Text("CPU").padding().colorInvert()
+                    }
+                }
+                Spacer()
+            }
+        }
     }
 }
 
