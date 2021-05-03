54329e2b017fdfeb5c6e7d8c11830c820c4c3f12
PrynsTag <princevelasco16@gmail.com>
2021-05-03 21:09:13 +0800

    Initial Commit

---
+++ Quick Start/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quick Start
+//
+//  Created by Prince Carl Velasco on 5/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quick Start/Quick_StartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_StartApp.swift
+//  Quick Start
+//
+//  Created by Prince Carl Velasco on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Quick_StartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
